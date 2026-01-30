9972022adfb53630526693180e5fb4e244d1dfeb
Bahar <baharalekberova05gmail.com>
2026-01-30 20:07:29 +0400

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./layouts/header.jsx";
+import Header from "./layouts/Header.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
